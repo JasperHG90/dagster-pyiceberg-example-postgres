7600cfe334b34e129df0850b3acf3831b582b90d
Jasper Ginn <jasperginn@gmail.com>
2024-11-02 21:42:51 +0100

    feat: add shortcut for creating namespace

---
+++ scripts/create_catalog_namespace.py
@@ -0,0 +1,17 @@
+import os
+
+from pyiceberg.catalog.sql import SqlCatalog
+
+if __name__ == "__main__":
+    catalog = SqlCatalog(
+        name="catalog",
+        **{
+            "uri": os.environ["DAGSTER_SECRET_PYICEBERG_CATALOG_URI"],
+            "s3.endpoint": os.environ["DAGSTER_SECRET_S3_ENDPOINT"],
+            "s3.access-key-id": os.environ["DAGSTER_SECRET_S3_ACCESS_KEY_ID"],
+            "s3.secret-access-key": os.environ["DAGSTER_SECRET_S3_SECRET_ACCESS_KEY"],
+            "py-io-impl": "pyiceberg.io.fsspec.FsspecFileIO",
+            "warehouse": os.environ["DAGSTER_SECRET_S3_WAREHOUSE"],
+        },
+    )
+    catalog.create_namespace_if_not_exists("air_quality")
