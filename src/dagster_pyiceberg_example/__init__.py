588d8bdaab5d27b8a8e893666beb1500a331f81b
Jasper Ginn <jasperginn@gmail.com>
2024-11-02 21:36:22 +0100

    feat: add pyiceberg IO manager

--- src/dagster_pyiceberg_example/__init__.py
+++ src/dagster_pyiceberg_example/__init__.py
@@ -1,4 +1,6 @@
-from dagster import Definitions, EnvVar, FilesystemIOManager
+from dagster import Definitions, EnvVar
+from dagster_pyiceberg import IcebergSqlCatalogConfig
+from dagster_pyiceberg_pandas import IcebergPandasIOManager
 
 from dagster_pyiceberg_example.assets import air_quality_data, daily_air_quality_data
 from dagster_pyiceberg_example.IO import (
@@ -8,7 +10,22 @@ from dagster_pyiceberg_example.IO import (
 )
 
 resources = {
-    "io_manager": FilesystemIOManager(),
+    "io_manager": IcebergPandasIOManager(
+        name="dagster_example_catalog",
+        config=IcebergSqlCatalogConfig(
+            properties={
+                "uri": EnvVar("DAGSTER_SECRET_PYICEBERG_CATALOG_URI"),
+                "s3.endpoint": EnvVar("DAGSTER_SECRET_S3_ENDPOINT"),
+                "s3.access-key-id": EnvVar("DAGSTER_SECRET_S3_ACCESS_KEY_ID"),
+                "s3.secret-access-key": EnvVar("DAGSTER_SECRET_S3_SECRET_ACCESS_KEY"),
+                "py-io-impl": "pyiceberg.io.fsspec.FsspecFileIO",
+                "warehouse": EnvVar("DAGSTER_SECRET_S3_WAREHOUSE"),
+            }
+        ),
+        schema="air_quality",
+        partition_spec_update_mode="error",
+        schema_update_mode="error",
+    ),
     "luchtmeetnet_api": LuchtMeetNetResource(
         rate_limiter=RateLimiterResource(  # See https://api-docs.luchtmeetnet.nl/ for rate limits
             rate_calls=100,
